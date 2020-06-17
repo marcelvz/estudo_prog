07e2d3002a2c17950238dd1a574f94cdb253999d
marcelvz <marcelv.zanardine@gmail.com>
2020-06-17 02:17:38 -0300

    Create equacaolinear.py

---
+++ equacaolinear.py
@@ -0,0 +1,12 @@
+#função que resolve uma equação linear ax+b=c
+
+def separa_fatores(equacao):
+  ab=equacao.rpartition("=")[0]
+  a=int(ab.rpartition("x")[0])
+  b=int(ab.rpartition("x")[-1])
+  c=int(equacao.rpartition("=")[-1])
+  x=(c-b)/a
+  print("valor de x=",x)
+
+
+separa_fatores(input("digite a equa\t").lower().replace(" ",""))
